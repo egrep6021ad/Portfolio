56cd796fc932820445aad3ebee2ded469428e344
Cal <khalfanijabari@gmail.com>
2022-05-01 19:56:56 -0400

    01 May - AboutME page (Mobile)

---
+++ src/components/About/Mobile/index.js
@@ -0,0 +1,27 @@
+import './index.scss'
+import logo from '../../../assets/images/logoWhole.png'
+const Mobile = () => {
+    return(
+        <div className='main-area'>
+           <h1>  Full-Stack <br />Developer </h1>
+            <h2> <img src={logo} alt="avatar" /> </h2> 
+           <main>
+           t is a long established fact that a reader will be <br />
+           distracted by the readable content of a page when looking <br />
+           at its layout. The point of using Lorem Ipsum is that it has a <br />
+           more-or-less normal distribution of letters, as opposed to <br />
+           using 'Content here, content here', making it look like <br />
+           readable English. Many desktop publishing packages and web <br />
+           page editors now use Lorem Ipsum as their default model text,<br />
+            and a search for 'lorem ipsum' will uncover many web sites <br />
+            still in their infancy. Various versions have evolved over 
+           </main>
+
+           <footer>
+        
+           </footer>
+
+        </div>
+    )
+}
+export default Mobile
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,32 +1,10 @@
-import './index.scss'
 
-const About = () => {
-    return(
-        <>
-        <div className='container about-page'>
-            <div className='text-area'>
-                <h1>Khalfani Bozeman</h1>
-                <h2>Subtitle</h2>
-               
-      
-                    <main>
-                       <p>hello</p>
-                       <img src='dffs' alt='what' />
-                    </main>
-               
-           
-                <footer>
-                       <p>hello 1</p>
-                       <p>hello 2</p>
-                       <p>hello 3</p>
-                       <p>hello 4</p>
-                       <p>hello 5</p>
-                </footer>
-                
-            </div>
-        </div>
-        </>
+import Mobile from './Mobile'
+import Web from './Web'
 
-    )
-}
+const frame = window.innerWidth
+
+const About = () => (
+    frame > 500 ?  <Web /> : <Mobile />
+)
 export default About
